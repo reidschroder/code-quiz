913d64a67700d64b126fab694b09f13616492fa9
Reid Schroder <reidschroder2@gmail.com>
2022-01-15 10:15:05 -0500

    added functions to display questions and scores, but not working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var timer = 75
 var quizTimer = document.querySelector("#quiz-timer")
 var questionIndex = 0
 var score = 0
-var questionBoxElement = document.getElementById('question-box')
+var questionBoxElement = document.querySelector('#question-box')
 
 var questions = [
     // add questions here!!
@@ -40,22 +40,75 @@ var questions = [
 ];
 // display questions
 function displayQuestion() {
-    let questions = questions[questionIndex];
+    let question = questions[questionIndex];
+    let answerOptions = question.options;
 
-    let questionEl = document.querySelector("question-box");
+    let questionEl = document.querySelector("#question-box");
     questionEl.textContent = question.questionText;
 
+    for (var i = 0; i < answerOptions.length; i++) {
+        let answerChoices = answerOptions[i];
+        let answerBtn = document.querySelector("#option"+i);
+        answerBtn.textContent = answerChoices;
+    }
 
-    questionIndex;
+
+    document.querySelector("#answer-buttons").addEventListener("click",checkAnswer);
+
+
+    // determine if content for selected button and correct answer is the same 
+    function correctAnswer(answerBtn){
+        return answerBtn.textContent === question[questionIndex].answer;
+    }
+    
+    // checkif answeris correct
+    function checkAnswer(event){
+        let answerBtn = event.target; 
+        // correct answer 
+        if (correctAnswer(answerBtn)){
+            score = score + 20;
+        }
+        // incorrect answer 
+        else {
+            if (timeLeft > 10){
+                timeLeft = timeLeft - 10;
+           }
+           else{
+               timeLeft = 0;
+               endQuiz();
+           }
+        }
+
+        questionIndex++;
+    
+        // if no more questions, end quiz
+        if (questionIndex < questions.length){
+            displayQuestion();
+        }
+        else{
+            endQuiz();
+        }
+    }
+    
+    document.querySelector("#answer-buttons").addEventListener("click", checkAnswer)
+
+
+
+
+
+
+
+
+
 }
 // timer countdown
 function countdown() {
     var timeLeft = setInterval(() => {
         
-        if (timer >= 0 && questionIndex < 5) {
+        if (timer >= 0 || questionIndex < 5) {
             quizTimer.textContent = timer + " seconds left";
             timer --;
-            questionCount ++;
+            questionIndex ++;
             
         }
         else {
